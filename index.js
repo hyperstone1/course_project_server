846a422892ba687c59bd4d8a20d573c9cdff0487
hyperstone1 <kingofthegorgon@mail.ru>
2023-01-09 18:31:30 +0300

    del static

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.static('public')); // удалить позже
-app.use(express.json({ limit: '50mb' })); // удалить позже
-
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
